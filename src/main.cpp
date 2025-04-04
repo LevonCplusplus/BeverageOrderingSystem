699c76efd3441d393d1c8a183b549f79d7c4e35a
Levon <levonlevonvarosyan@gmail.com>
2025-04-05 03:42:42 +0400

    without tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ int main(){
     std::shared_ptr<Beverage> bev1 = std::make_shared<Espresso>();
     std::shared_ptr<Beverage> bev2 = std::make_shared<Late>();   
     std::shared_ptr<Beverage> bev3 = std::make_shared<GreenTea>();
+    std::shared_ptr<Beverage> bev4 = std::make_shared<BlackTea>();
+
     menu -> addBeveragetoMenu(bev1);
     menu -> addBeveragetoMenu(bev2);
     menu -> addBeveragetoMenu(bev3);
